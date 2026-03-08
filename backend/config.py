2afa27c6aff2f6d364374eb13ff2f4e0e2558343
HashBukhtiar <hashbukhtiar@gmail.com>
2026-03-07 21:49:55 -0500

    added extra: ignore to model config

--- backend/config.py
+++ backend/config.py
@@ -55,7 +55,7 @@ class Settings(BaseSettings):
     port: int = 8000
     patient_name: str = "Dad"
 
-    model_config = {"env_file": "../.env", "env_file_encoding": "utf-8"}
+    model_config = {"env_file": "../.env", "env_file_encoding": "utf-8", "extra": "ignore"}
 
 
 settings = Settings()
